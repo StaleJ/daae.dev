a4b6b5a5192350d186aa3b19eb7456f120a42972
Petter Daae <petter.daae@gmail.com>
2019-05-11 23:04:08 +0200

    add htb badge and change colors

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,6 +8,7 @@ function Home() {
       <div className="content">
         <GithubLogo className="github github-big spinning"></GithubLogo>
         <div className="welcome">Hi, I'm Petter. I'm studying computer tehcnology at <b>UiB</b> and working part-time as a junior developer in <b>Sbanken</b>.</div>
+        <img src="https://www.hackthebox.eu/badge/image/71349" alt="Hack The Box" class="htb"></img>
       </div>
     </div>
   );
